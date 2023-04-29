a1349841e0efae0b35c7fa11c7400fac56590637
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-04-29 17:24:56 +0100

    Reubicar ImagePikcer en la carpeta Helper. Crear CameraPicker. Modificar Info para añadir permisos para la cámara

---
+++ Model/Helpers/CameraPicker.swift
@@ -0,0 +1,45 @@
+//
+//  CameraPicker.swift
+//  MyLibrary
+//
+//  Created by Javier Rodríguez Gómez on 29/4/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct CameraPicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.dismiss) var dismiss
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraPicker
+        
+        init(_ parent: CameraPicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
