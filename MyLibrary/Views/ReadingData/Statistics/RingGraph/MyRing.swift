0343cbe0b4152dc762832c01e8a6ba3a053cc535
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-08-09 23:59:26 +0100

    Reordering some files related to RD graphs and charts.
    RingCircleArc.
    MyRing.
    LineChart.
    NewLineGraph.
    OldLineGraph.
    OldLineGraphExt.

--- MyLibrary/Views/ReadingData/Statistics/RingGraph/MyRing.swift
+++ MyLibrary/Views/ReadingData/Statistics/RingGraph/MyRing.swift
@@ -1,38 +1,11 @@
 //
-//  RingCircleArc.swift
+//  MyRing.swift
 //  MyLibrary
 //
-//  Created by Javier Rodríguez Gómez on 15/4/22.
+//  Created by Javier Rodríguez Gómez on 9/8/23.
 //
 
 import SwiftUI
-
-struct RingCircleArc: View {
-    let datas: [Int]
-    let colors: [Color]
-    
-    var body: some View {
-        ZStack {
-            ForEach(datas.indices, id:\.self) { index in
-                MyRing(datas: datas, index: index)
-                    .foregroundColor(colors[index])
-            }
-        }
-        .scaleEffect(0.7)
-        .frame(maxWidth: .infinity)
-        .padding()
-    }
-}
-
-struct RingCircleArc_Previews: PreviewProvider {
-    static let datas = [10, 20, 30, 40]
-    static let colors: [Color] = [.red, .orange, .blue, .green, .yellow]
-    
-    static var previews: some View {
-        RingCircleArc(datas: datas, colors: colors)
-    }
-}
-
 
 struct MyRing: View {
     @State private var show = false
