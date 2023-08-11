2fff3dc7ef09552777e07ca959347b2bc79799ad
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-08-11 12:35:17 +0100

    Refactoring AddRS with extension for computed properties, methods and modifiers.

--- MyLibrary/Views/ReadingList/ReadingSessions/AddRS.swift
+++ MyLibrary/Views/ReadingList/ReadingSessions/AddRS.swift
@@ -17,40 +17,20 @@ struct AddRS: View {
     
     @Binding var book: NowReading
     
-    @State private var sessionDate: Date = .now
+    @State var sessionDate: Date = .now
     @State var startingPage: Int
-    @State private var endingPage: Int = 0
+    @State var endingPage: Int = 0
     @State var hour: Int
     @State var minute: Int
-    @FocusState private var isFocused: Bool
-    @State private var addingComment = false
-    @State private var commentText = ""
+    @FocusState var isFocused: Bool
+    @State var addingComment = false
+    @State var commentText = ""
     
-    @State private var showingDailyAchivedAlert = false
-    @State private var showingWeeklyAchivedAlert = false
+    @State var showingDailyAchivedAlert = false
+    @State var showingWeeklyAchivedAlert = false
     
-    @State private var existingTime = 0
-    @State private var showingExistingTimeAlert = false
-    
-    var sessionDuration: String {
-        return "\(hour)h \(minute)min"
-    }
-    var minPerPag: String {
-        let valueInMinutes = durationInMinutes / Double(endingPage - startingPage + 1)
-        let float = valueInMinutes.truncatingRemainder(dividingBy: 1.0)
-        let min = Int(valueInMinutes - float)
-        let sec = Int(round(float * 60))
-        return "\(min)min \(sec)s"
-    }
-    var durationInMinutes: Double {
-        Double(hour * 60 + minute)
-    }
-    
-    var isDisabled: Bool {
-        guard startingPage == 0 || endingPage == 0 || startingPage > endingPage ||
-        (hour == 0 && minute == 0) else { return false }
-        return true
-    }
+    @State var existingTime = 0
+    @State var showingExistingTimeAlert = false
     
     var body: some View {
         Form {
@@ -128,8 +108,7 @@ struct AddRS: View {
                 }
             }
         }
-        .navigationTitle(book.bookTitle)
-        .navigationBarTitleDisplayMode(.inline)
+		.modifier(AddRSModifier(book: $book, hour: $hour, minute: $minute, existingTime: $existingTime, showingDailyAchivedAlert: $showingDailyAchivedAlert, showingWeeklyAchivedAlert: $showingWeeklyAchivedAlert, showingExistingTimeAlert: $showingExistingTimeAlert))
         .toolbar {
             ToolbarItem(placement: .cancellationAction) {
                 Button("Cancelar") {
@@ -148,67 +127,6 @@ struct AddRS: View {
                     isFocused = false
                 }
             }
-        }
-        .alert("¡Enhorabuena!", isPresented: $showingDailyAchivedAlert) {
-            Button("OK") { dismiss() }
-        } message: {
-            Text("Has alcanzado tu objetivo diario de lectura.")
-        }
-        .alert("¡Enhorabuena!", isPresented: $showingWeeklyAchivedAlert) {
-            Button("OK") { dismiss() }
-        } message: {
-            Text("Has alcanzado tu objetivo semanal de lectura.")
-        }
-        .alert("Existe una sesión iniciada en tu AppleWatch.", isPresented: $showingExistingTimeAlert) {
-            Button("Descartar", role: .cancel) {
-                wcmodel.readingTime = 0
-            }
-            Button("Añadir") {
-                hour += existingTime / 60
-                minute += existingTime % 60
-                wcmodel.readingTime = 0
-            }
-        } message: {
-            Text("¿Deseas sumar la duración de esa sesión?")
-        }
-        .onAppear {
-            existingTime = wcmodel.readingTime
-            if existingTime != 0 {
-                showingExistingTimeAlert = true
-            }
-        }
-    }
-    
-    func addSession() {
-        guard let index = nrmodel.readingList.firstIndex(of: book) else { return }
-        if endingPage >= book.lastPage {
-            endingPage = book.lastPage
-            nrmodel.readingList[index].isFinished = true
-        }
-        let newDate: Date = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: sessionDate) ?? .now
-        var newSession = ReadingSession(id: UUID(), date: newDate, duration: sessionDuration, startingPage: startingPage, endingPage: endingPage, minPerPag: minPerPag)
-        if !commentText.isEmpty {
-            newSession.comment = Quote(date: sessionDate, bookTitle: book.bookTitle, page: 0, text: commentText)
-        }
-        if !rsmodel.tempQuotesArray.isEmpty {
-            newSession.quotes = rsmodel.tempQuotesArray
-            rsmodel.tempQuotesArray.removeAll()
-        }
-        nrmodel.readingList[index].sessions.insert(newSession, at: 0)
-        rsmodel.readingSessionList.insert(newSession, at: 0)
-        
-        if Calendar.current.component(.weekday, from: newDate) == 7 {
-            let weekPages = rsmodel.calcTotalPagesPerWeekAndMonth(tag: 0).pages.reduce(0,+)
-            let weekSessions = rsmodel.getSessions(tag: 0)
-            let weekTime = weekSessions.reduce(0) { $0 + $1.readingTimeInHours }
-            if weekPages >= model.weeklyPagesTarget || weekTime >= model.weeklyTimeTarget {
-                showingWeeklyAchivedAlert = true
-            }
-        }
-        if newSession.pages >= model.dailyPagesTarget || newSession.readingTimeInHours >= model.dailyTimeTarget {
-            showingDailyAchivedAlert = true
-        } else {
-            dismiss()
         }
     }
 }
---
+++ MyLibrary/Views/ReadingList/ReadingSessions/AddRSExt.swift
@@ -0,0 +1,110 @@
+//
+//  AddRSExt.swift
+//  MyLibrary
+//
+//  Created by Javier Rodríguez Gómez on 11/8/23.
+//
+
+import SwiftUI
+
+extension AddRS {
+	var sessionDuration: String {
+		return "\(hour)h \(minute)min"
+	}
+	var minPerPag: String {
+		let valueInMinutes = durationInMinutes / Double(endingPage - startingPage + 1)
+		let float = valueInMinutes.truncatingRemainder(dividingBy: 1.0)
+		let min = Int(valueInMinutes - float)
+		let sec = Int(round(float * 60))
+		return "\(min)min \(sec)s"
+	}
+	var durationInMinutes: Double {
+		Double(hour * 60 + minute)
+	}
+	
+	var isDisabled: Bool {
+		guard startingPage == 0 || endingPage == 0 || startingPage > endingPage ||
+				(hour == 0 && minute == 0) else { return false }
+		return true
+	}
+	
+	func addSession() {
+		guard let index = nrmodel.readingList.firstIndex(of: book) else { return }
+		if endingPage >= book.lastPage {
+			endingPage = book.lastPage
+			nrmodel.readingList[index].isFinished = true
+		}
+		let newDate: Date = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: sessionDate) ?? .now
+		var newSession = ReadingSession(id: UUID(), date: newDate, duration: sessionDuration, startingPage: startingPage, endingPage: endingPage, minPerPag: minPerPag)
+		if !commentText.isEmpty {
+			newSession.comment = Quote(date: sessionDate, bookTitle: book.bookTitle, page: 0, text: commentText)
+		}
+		if !rsmodel.tempQuotesArray.isEmpty {
+			newSession.quotes = rsmodel.tempQuotesArray
+			rsmodel.tempQuotesArray.removeAll()
+		}
+		nrmodel.readingList[index].sessions.insert(newSession, at: 0)
+		rsmodel.readingSessionList.insert(newSession, at: 0)
+		
+		if Calendar.current.component(.weekday, from: newDate) == 7 {
+			let weekPages = rsmodel.calcTotalPagesPerWeekAndMonth(tag: 0).pages.reduce(0,+)
+			let weekSessions = rsmodel.getSessions(tag: 0)
+			let weekTime = weekSessions.reduce(0) { $0 + $1.readingTimeInHours }
+			if weekPages >= model.weeklyPagesTarget || weekTime >= model.weeklyTimeTarget {
+				showingWeeklyAchivedAlert = true
+			}
+		}
+		if newSession.pages >= model.dailyPagesTarget || newSession.readingTimeInHours >= model.dailyTimeTarget {
+			showingDailyAchivedAlert = true
+		} else {
+			dismiss()
+		}
+	}
+	
+	struct AddRSModifier: ViewModifier {
+		@ObservedObject var wcmodel = ConnectivityMaganer()
+		@Environment(\.dismiss) var dismiss
+		
+		@Binding var book: NowReading
+		@Binding var hour: Int
+		@Binding var minute: Int
+		@Binding var existingTime: Int
+		@Binding var showingDailyAchivedAlert: Bool
+		@Binding var showingWeeklyAchivedAlert: Bool
+		@Binding var showingExistingTimeAlert: Bool
+		
+		func body(content: Content) -> some View {
+			content
+				.navigationTitle(book.bookTitle)
+				.navigationBarTitleDisplayMode(.inline)
+				.alert("¡Enhorabuena!", isPresented: $showingDailyAchivedAlert) {
+					Button("OK") { dismiss() }
+				} message: {
+					Text("Has alcanzado tu objetivo diario de lectura.")
+				}
+				.alert("¡Enhorabuena!", isPresented: $showingWeeklyAchivedAlert) {
+					Button("OK") { dismiss() }
+				} message: {
+					Text("Has alcanzado tu objetivo semanal de lectura.")
+				}
+				.alert("Existe una sesión iniciada en tu AppleWatch.", isPresented: $showingExistingTimeAlert) {
+					Button("Descartar", role: .cancel) {
+						wcmodel.readingTime = 0
+					}
+					Button("Añadir") {
+						hour += existingTime / 60
+						minute += existingTime % 60
+						wcmodel.readingTime = 0
+					}
+				} message: {
+					Text("¿Deseas sumar la duración de esa sesión?")
+				}
+				.onAppear {
+					existingTime = wcmodel.readingTime
+					if existingTime != 0 {
+						showingExistingTimeAlert = true
+					}
+				}
+		}
+	}
+}
