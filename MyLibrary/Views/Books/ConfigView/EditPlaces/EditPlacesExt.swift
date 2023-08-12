111a51108a7d1732cd3fc1c06ede19e24227b4ce
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-08-12 01:03:41 +0100

    Refactoring EditPlaces with extensions for methods, view modifiers, and subviews as computed properties.
    Adding EditPlacesExt and EditPlacesSection1.

---
+++ MyLibrary/Views/Books/ConfigView/EditPlaces/EditPlacesExt.swift
@@ -0,0 +1,77 @@
+//
+//  EditPlacesExt.swift
+//  MyLibrary
+//
+//  Created by Javier Rodríguez Gómez on 12/8/23.
+//
+
+import SwiftUI
+
+extension EditPlaces {
+	func move(from source: IndexSet, to destination: Int) {
+		model.myPlaces.move(fromOffsets: source, toOffset: destination)
+	}
+	
+	// View modifier para la lista con places
+	struct PlacesListModifier: ViewModifier {
+		@EnvironmentObject var model: UserViewModel
+		@EnvironmentObject var bmodel: BooksModel
+		
+		@Binding var oldPlace: String
+		@Binding var newPlace: String
+		@Binding var showingAddPlace: Bool
+		@Binding var showingEditPlace: Bool
+		@Binding var showingDeleteAlert: Bool
+		@Binding var showingEditAlert: Bool
+		
+		func body(content: Content) -> some View {
+			content
+				.toolbar {
+					EditButton()
+						.disabled(showingAddPlace || showingEditPlace)
+				}
+				.alert("Esta ubicación tiene libros registrados.\n¿Deseas borrarla de todas formas?", isPresented: $showingDeleteAlert) {
+					Button("No", role: .cancel) { }
+					Button("Sí", role: .destructive) {
+						model.myPlaces.removeAll(where: { $0 == oldPlace })
+						bmodel.moveFromTo(from: oldPlace, to: "sin asignar")
+						oldPlace = ""
+					}
+				} message: {
+					Text("Esto NO borrará los libros registrados.")
+				}
+				.alert("Se ha modificado el nombre de esta ubicación.", isPresented: $showingEditAlert) {
+					Button("No", role: .cancel) { }
+					Button("Sí") {
+						bmodel.moveFromTo(from: oldPlace, to: newPlace)
+						newPlace = ""
+					}
+				} message: {
+					Text("¿Quieres cambiar también la ubicación en todos los registros?")
+				}
+		}
+	}
+	
+	// View modifier para la vista global
+	struct EditPlacesModifier: ViewModifier {
+		@Binding var showingAddPlace: Bool
+		@Binding var showingEditPlace: Bool
+		
+		func body(content: Content) -> some View {
+			content
+				.autocorrectionDisabled()
+				.navigationTitle("Ubicaciones")
+				.navigationBarTitleDisplayMode(.inline)
+				.toolbar {
+					ToolbarItem(placement: .navigationBarTrailing) {
+						Button() {
+							showingAddPlace = true
+						} label: {
+							Label("Añadir", systemImage: "plus")
+						}
+						.disabled(showingAddPlace || showingEditPlace)
+					}
+				}
+		}
+	}
+}
