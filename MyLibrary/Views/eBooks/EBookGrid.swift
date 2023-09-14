16a9738ed5fa8b178c871f9c86a24e5d81900745
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-09-14 18:19:39 +0100

    Implementing selection between list and grid view for ebooks.
    Modifying EBookList.
    Adding EBookGrid.

---
+++ MyLibrary/Views/eBooks/EBookGrid.swift
@@ -0,0 +1,20 @@
+//
+//  EBookGrid.swift
+//  MyLibrary
+//
+//  Created by Javier Rodríguez Gómez on 14/9/23.
+//
+
+import SwiftUI
+
+struct EBookGrid: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct EBookGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        EBookGrid()
+    }
+}
--- MyLibrary/Views/eBooks/EBookList.swift
+++ MyLibrary/Views/eBooks/EBookList.swift
@@ -9,21 +9,40 @@ import SwiftUI
 
 struct EBookList: View {
     @EnvironmentObject var model: UserViewModel
+	@State var showingGridWithCovers = false
     @State var searchText = ""
     let filter: FilterByStatus
     var filteredOwner: String? = nil
     
     var body: some View {
-        List(ebooksToShow) { ebook in
-			let index = model.user.ebooks.firstIndex(of: ebook)!
-			NavigationLink(destination: EBookDetail(ebook: $model.user.ebooks[index], newStatus: ebook.status)) {
-                EBookRow(ebook: ebook)
-            }
+        NavigationStack {
+			if showingGridWithCovers {
+				EBookGrid()
+			} else {
+				List(ebooksToShow) { ebook in
+					let index = model.user.ebooks.firstIndex(of: ebook)!
+					NavigationLink(destination: EBookDetail(ebook: $model.user.ebooks[index], newStatus: ebook.status)) {
+						EBookRow(ebook: ebook)
+					}
+				}
+				.searchable(text: $searchText, prompt: "Búsqueda por título o autor")
+				.disableAutocorrection(true)
+			}
         }
         .navigationTitle(navigationTitle)
         .navigationBarTitleDisplayMode(.inline)
-        .searchable(text: $searchText, prompt: "Búsqueda por título o autor")
-        .disableAutocorrection(true)
+		.toolbar {
+			ToolbarItem(placement: .navigationBarTrailing) {
+				Button {
+					withAnimation {
+						showingGridWithCovers.toggle()
+					}
+				} label: {
+					Image(systemName: showingGridWithCovers ? "list.star" : "square.grid.3x3")
+				}
+
+			}
+		}
     }
 }
 
