984a6e5f9395a49ef8c3b5cd99fbad59cb3bc2bb
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-10-19 19:15:46 +0100

    Writing documentation for PersistenceInteractor

--- Model/Interactor/PersistenceInteractor.swift
+++ Model/Interactor/PersistenceInteractor.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+/// A protocol to handle the app persistence.
+///
+/// It has two properties: one for the Bundle url and one for the sandbox url.
+/// It also has two methods: one for loading data from either the bundle or the sandbox, and one for saving data to sandbox.
+///
+/// With this protocol we can handle preview data and production data, by defining or directioning ``bundleURL`` and ``docURL`` to one or the other.
 protocol PersistenceInteractor {
+	/// The url to the Bundle where the data originally exists.
 	var bundleURL: URL { get }
+	/// The url to the sandbox where the data can be saved and/or exists.
 	var docURL: URL { get }
 	
+	/// A method to load data from either the Bundle or the sandbox.
+	/// - Returns: A ``User`` instance containing the main data stored either in the Bundle or in the sandbox.
 	func loadUser() throws -> User
+	/// A method to store all the main data to sandbox.
+	/// - Parameter user: A ``User`` instance containing all the main data.
 	func saveUser(_ user: User) throws
 }
 
@@ -36,6 +48,7 @@ extension PersistenceInteractor {
 
 // Persistencia para producción
 
+/// A struct conforming to ``PersistenceInteractor`` to handle the persistence of production data.
 struct Persistence: PersistenceInteractor {
 	var bundleURL: URL {
 		Bundle.main.url(forResource: "USERDATA", withExtension: "json")!
