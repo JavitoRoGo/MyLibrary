dcca616e7e2af875f6a46fdc329522b72a1a273a
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2023-10-19 19:48:34 +0100

    Writing documentation for UserLogic

--- Model/ModelLogic/UserLogic.swift
+++ Model/ModelLogic/UserLogic.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 @Observable
+/// A class to handle all the logic for User production and preview data.
+///
+/// It conforms to the new Observation API.
 final class UserLogic {
 	// Patrón singleton: siempre se llamará al shared
+	/// A static property that belongs to the class and uses the singleton pattern.
 	static let shared = UserLogic()
 	
+	/// A property to contain the persistence type, so it can handle preview and production data.
 	let persistence: PersistenceInteractor
 	
 	// Propiedades Published
+	/// The main data of the app.
 	var user: User {
 		didSet {
 			do {
@@ -37,8 +43,11 @@ final class UserLogic {
 		}
 	}
 	
+	/// A property used to temporally store all the quotes created during a reading session, so they are ready to be saved when creating the new reading session at the end.
 	var tempQuotesArray: [Quote] = []
 	
+	/// The init method to initialise the ``persistence`` class property.
+	/// - Parameter persistence: If not defined, by default it is initialised as production data persistence type.
 	init(persistence: PersistenceInteractor = Persistence()) {
 		// Valor de persistencia por defecto para los datos de producción
 		self.persistence = persistence
